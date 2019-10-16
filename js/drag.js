3b73534699ec2586d27b512286c1680b996f1b1d
bonan <zbn.dale@gmail.com>
2019-10-16 14:27:41 +0100

    :lipstick: set button-group class

--- js/drag.js
+++ js/drag.js
@@ -2,11 +2,11 @@
 // Tuning configs
 const boxAreaMargin = 30;
 const dragElementKeyword = 'stone';
-let itemPos = stones; // JSON in js format, load as script
+const itemPos = stones; // JSON in js format, load as script
 
 // Initialize stone attributes
 let dragItem;
-let dragItemPos = itemPos['stone1']; // default value
+let dragItemPos = itemPos['stone1']; // Pick a default value to avoid console warnings
 
 // Set up event listeners
 const container = document.querySelector(".box");
@@ -24,12 +24,11 @@ const containerPos = container.getBoundingClientRect();
 
 const topLimit = Math.round(containerPos.top) + boxAreaMargin;
 const bottomLimit = Math.round(containerPos.bottom) - boxAreaMargin;
-const rightLimit = Math.round(containerPos.right) - boxAreaMargin;
 const leftLimit = Math.round(containerPos.left) + boxAreaMargin;
-
+const rightLimit = Math.round(containerPos.right) - boxAreaMargin;
 
 // Dragging functions
-// See https://www.kirupa.com/html5/drag.htm
+// Adapted from https://www.kirupa.com/html5/drag.htm
 function dragStart(e) {
   if (e.target.id.indexOf(dragElementKeyword) > -1) {
     dragItem = e.target;
@@ -37,7 +36,8 @@ function dragStart(e) {
     dragItemPos.active = true;
   }
 
-  let clientPos = (e.type === "touchstart") ? e.touches[0] : e ;
+  // Pick touch-screen friendly action
+  const clientPos = (e.type === "touchstart") ? e.touches[0] : e ;
   dragItemPos.initialX = clientPos.clientX - dragItemPos.xOffset;
   dragItemPos.initialY = clientPos.clientY - dragItemPos.yOffset;
   
@@ -47,17 +47,17 @@ function drag(e) {
   if (dragItemPos.active) {
     e.preventDefault();
 
-    let clientPos = (e.type === "touchmove") ? e.touches[0] : e ;
-    let isOutside = (clientPos.clientY < topLimit) || (clientPos.clientY > bottomLimit) ||
-                    (clientPos.clientX < leftLimit) || (clientPos.clientX > rightLimit)
+    const clientPos = (e.type === "touchmove") ? e.touches[0] : e ;
     
+    // Restrict movements in the draggable area
+    const isOutside = (clientPos.clientY < topLimit) || (clientPos.clientY > bottomLimit) ||
+                      (clientPos.clientX < leftLimit) || (clientPos.clientX > rightLimit)
     if (!isOutside) {
       dragItemPos.currentX = clientPos.clientX - dragItemPos.initialX;
       dragItemPos.currentY = clientPos.clientY - dragItemPos.initialY;
-    } else {
-      dragEnd(e);
     }
     
+    // Track movements
     dragItemPos.xOffset = dragItemPos.currentX;
     dragItemPos.yOffset = dragItemPos.currentY;
     setTranslate(dragItemPos.currentX, dragItemPos.currentY, dragItem);
