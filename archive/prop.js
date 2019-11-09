2ae900bf7b6d216d2a35faf95bcd5e859f62400f
bonan <zbn.dale@gmail.com>
2019-11-09 00:47:52 +0000

    :pencil2: chore: Archive old versions

---
+++ archive/prop.js
@@ -0,0 +1,51 @@
+
+// Define custom colors
+const myColors = {
+    red: 'rgb(255, 102, 102)',
+    blue: 'rgb(61, 60, 134)',
+    yellow: 'rgb(245, 230, 99)'
+  }
+const magicStoneBorderStyle = '10px solid rgba(136, 136, 136, .5)';
+
+// Default stone attributes
+const stones = {
+    "stone1": {
+      "id": 'stone1',
+      "type": 'magic',
+      "defaultColor": myColors.blue,
+    },
+    "stone2": {
+      "id": 'stone2',
+      "type": 'normal',
+      "defaultColor": myColors.yellow,
+    }
+}
+
+// Initialize magic rules
+let humanReadableColorRules = {};
+let humanReadableColorShapeRules = {};
+let humanReadableColorSizeRules = {};
+let humanReadableShapeColorRules = {};
+let humanReadableSizeColorRules = {}
+
+// Define transform rules here
+humanReadableColorRules = {
+    'blue': 'red',
+    'yellow': 'red',
+}
+
+humanReadableColorSizeRules = {
+  'yellow': '80',
+}
+
+humanReadableSizeColorRules = {
+  '80': 'blue',
+}
+
+humanReadableColorShapeRules = {
+  'blue': 'diamond',
+}
+
+humanReadableShapeColorRules = {
+  '20%': 'yellow',
+}
