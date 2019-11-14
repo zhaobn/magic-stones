01b534f1b7ae9217ae233a05691324108420ef18
bonan <zbn.dale@gmail.com>
2019-11-14 14:41:02 +0000

    :sparkles: feat: Show learning task before each trial

--- js/task.js
+++ js/task.js
@@ -1,8 +1,52 @@
 
+const learningTask = trainings.learn04;
+
+/** Create learning task stones */
+createStones(learningTask);
+
+/** Set learning task button functions */
+const playBtn = document.getElementById('play-btn');
+const resetBtn = document.getElementById('reset-btn');
+const nextBtn = document.getElementById('next-btn');
+
+resetBtn.onclick = () => resetStones(learningTask);
+
+playBtn.onclick = () => {
+    playEffects(learningTask);
+    setTimeout(() => {
+        resetBtn.disabled = false;
+        nextBtn.disabled = false
+    }, 2000);
+};
+
+nextBtn.onclick = () => {
+    document.getElementById('learning').style.display = 'none';
+    document.getElementById('task').style.display = 'inline';
+    document.getElementById('trial').scrollIntoView({
+        behavior: 'smooth'
+    });
+    resetStones(learningTask);
+    nextBtn.disabled = true;
+    resetBtn.disabled = true;
+};
+
+/** Create history stones */
 effectsHistory(learningTask);
+
+/** Create generalization tasks */
 createDataObj(learningTask);
 
 let trial = 'trial01';
 createGeneralizationTask(taskData[trial]);
+
+const nextOneButton = document.getElementById('next-one-btn');
+
+nextOneButton.onclick = () => {
+    document.getElementById('learning').style.display = 'inline';
+    document.getElementById('task').style.display = 'none';
+    updateTask(trial);
+    nextOneButton.disabled = true;
+}
+
 
 document.getElementById('next-one-btn').onclick = () => updateTask(trial);
