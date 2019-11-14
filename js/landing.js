6022bdcf172e6a40cb7b2a59a5b6f589db49638d
bonan <zbn.dale@gmail.com>
2019-11-14 15:46:53 +0000

    :sparkles: feat: Merge descriptions into one page

---
+++ js/landing.js
@@ -0,0 +1,54 @@
+
+const consentBtn = document.getElementById('consent-btn');
+const reminderBtn = document.getElementById('reminder-btn');
+const descriptionBtn = document.getElementById('desc-btn');
+
+const checkBtn = document.getElementById('check-btn');
+const checks = [ 'check1', 'check2', 'check3', 'check4' ];
+const answers = [ true, false, true, true ];
+
+const passBtn = document.getElementById('pass-btn');
+const retryBtn = document.getElementById('retry-btn');
+
+consentBtn.onclick = () => {
+    document.getElementById('landing').style.display = 'none';
+    document.getElementById('reminder').style.display = 'block';
+}
+
+reminderBtn.onclick = () => {
+    document.getElementById('reminder').style.display = 'none';
+    document.getElementById('description').style.display = 'block';
+    document.body.className = 'dark-page';
+}
+
+descriptionBtn.onclick = () => {
+    document.getElementById('description').style.display = 'none';
+    document.getElementById('comprehension').style.display = 'block';
+    document.getElementById('check-btn').style.display = 'block';
+    document.body.className = 'light-page';
+}
+
+checkBtn.onclick = () => checkComprehension();
+passBtn.onclick = () => location.href='task.html';
+retryBtn.onclick = () => {
+    document.getElementById('comprehension').style.display = 'none';
+    document.getElementById('retry').style.display = 'none';
+    document.getElementById('description').style.display = 'block';
+    document.body.className = 'dark-page';
+};
+
+function checkComprehension() {
+    let inputs = [];
+    checks.map(check => {
+        const vals = document.getElementsByName(check);
+        inputs.push(vals[0].checked);
+    });
+    const pass = (inputs.join('') === answers.join(''));
+    showPostCheckPage(pass);
+}
+
+function showPostCheckPage (isPass) {
+    const pageDiv = isPass? 'pass' : 'retry';
+    document.getElementById('check-btn').style.display = 'none';
+    document.getElementById(pageDiv).style.display = 'block';
+}
