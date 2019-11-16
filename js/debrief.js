263fa848139f1adec4a4393074f70bf08593d009
bonan <zbn.dale@gmail.com>
2019-11-16 11:44:54 +0000

    :sparkles: feat: Collect debrief data

--- js/debrief.js
+++ js/debrief.js
@@ -1,6 +1,25 @@
 
-/** Sumit button: save data */
-document.getElementById('submit-btn').onclick = () => saveData();
+const doneBtn = document.getElementById('done-btn');
+const debriefForm = document.getElementById('postquiz');
+
+debriefForm.onchange = () => {
+  isFilled('postquiz')? doneBtn.disabled = false: null;
+}
+doneBtn.onclick = () => saveData();
+
+/** Check if form is filled */
+/** Return TRUE if form is fully filled */
+function isFilled (formID) {
+  let notFilled = false;
+  const nulls = [ '', '', 'noresp', '--', '--' ];
+  const form = document.getElementById(formID);
+  const inputs = form.elements;
+  (Object.keys(inputs)).forEach((s, i) => {
+    let input = inputs[s].value;
+    notFilled = (notFilled || (input === nulls[i]));
+  });
+  return (!notFilled)
+}
 
 /** Auto-download data file for off-line use */
 function download(content, fileName, contentType) {
