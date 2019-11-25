daec7588f2edcb7df10631daae6268026d629c78
bonan <zbn.dale@gmail.com>
2019-11-25 15:44:11 +0000

    :pencil: [analysis] Clean non-pilot data

--- analysis/pilot_20191121_6group15trials.r
+++ analysis/pilot_20191121_6group15trials.r
@@ -5,7 +5,8 @@ library(rjson)
 rm(list=ls())
 
 exp = 'bn_magic_stones'
-n_tests = 6
+pilot_start = 6
+pilot_end = 11
 
 taskTableName = paste0(exp, '_', "task")
 participantTableName = paste0(exp, '_', "participant")
@@ -27,9 +28,9 @@ dbExistsTable(con, participantTableName)
 
 # Then you can pull the task data from postgreSQL 
 td <- dbGetQuery(con, paste("SELECT * from ", taskTableName))
-#Here it is as json
-td$subject
-td$trials
+# Trim off test data
+subject<-td$subject[c(pilot_start:pilot_end)]
+trials<-td$trials[c(pilot_start:pilot_end)]
 
 #Un-jsonify it
 inv_fromJSON<-function(js)
@@ -41,30 +42,30 @@ inv_fromJSON<-function(js)
 # Drop clicks data for now because I don't know how to process them
 # TODO: analyze clicks data
 drop_clicks <- function(text) {
-  return (paste0(substr(x, 0, 399), '}"'))
+  return (paste0(substr(text, 0,605), '}"'))
 }
 
 #and turn each subject into a dataframe
 ### Manual debug..
-td$subject[7] = sub("\"Let's talk in person\"", "'Lets talk in person'", td$subject[7])
-sw<-sapply(sapply(td$subject, inv_fromJSON, simplify=F), as.data.frame, simplify=F)
+subject[2] = sub("\"Let's talk in person\"", "'Lets talk in person'", subject[2])
+sw<-sapply(sapply(subject, inv_fromJSON, simplify=F), as.data.frame, simplify=F)
 # tw<-sapply(sapply(td$trials, inv_fromJSON, simplify=F), as.data.frame, simplify=F)
-tw<-sapply(sapply(sapply(td$trials, drop_clicks), inv_fromJSON, simplify=F), as.data.frame, simplify=F)
+tw<-sapply(sapply(sapply(trials, drop_clicks), inv_fromJSON, simplify=F), as.data.frame, simplify=F)
 
-N<-length(tw[[1]]$trial)
+N<-length(tw[[1]]$trial) # 15 trials
 
 #Combine them
-df.sw.aux<-sw[[n_tests]]
-df.tw.aux<-tw[[n_tests]]
-for (i in (n_tests+1):length(sw)) {
+df.sw.aux<-sw[[1]]
+df.tw.aux<-tw[[1]]
+for (i in 2:length(sw)) {
   df.sw.aux<-rbind(df.sw.aux, sw[[i]])
   df.tw.aux<-rbind(df.tw.aux, tw[[i]])
 }
 #And append them to the id and upis
 df.sw<-data.frame(ix=td$id,
                   id=td$participant)
-df.sw <- df.sw[c(n_tests:nrow(df.sw)),]
+df.sw <- df.sw[c(pilot_start:pilot_end),]
 df.sw<-cbind(df.sw, df.sw.aux)
 df.tw<-cbind(ix=rep(df.sw$ix, each=N), id=rep(df.sw$id, each=N), df.tw.aux)
 
-save(file='../data/pilot_20191121', df.sw, df.tw)
+save(file='../data/pilot_20191121.Rdata', df.sw, df.tw)
