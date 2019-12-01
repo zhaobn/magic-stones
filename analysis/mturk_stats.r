7a9fb7b3b5c25627bbd523014c5617f1a90ec508
bonan <zbn.dale@gmail.com>
2019-12-01 17:21:24 +0000

    :white_check_mark: Update mturk stats using fixed data

--- analysis/mturk_stats.r
+++ analysis/mturk_stats.r
@@ -5,7 +5,7 @@ library(dplyr)
 rm(list=ls())
 
 # Load data
-load('../data/mturk_20191128.Rdata')
+load('../data/mturk_20191128_fixed.Rdata')
 
 # Ensure correct data type
 df.sw$sex <- as.character(df.sw$sex)
@@ -42,60 +42,66 @@ summarise_group <- function(list, ndigits=2, separator='|') {
 # Subjectwise basic stats
 # Age
 report_col(df.sw$age)
-# min: 24 | max: 58 | mean: 38.8235 | sd: 9.9376
+# min: 24 | max: 64 | mean: 41.0882 | sd: 11.4452 
 
 # Sex
 summarise_group(df.sw$sex)
-# total 34 | male 16 47.06% | female 18 52.94%
+# total 34 | male 17 50% | female 17 50%
 genders <- unique(df.sw$sex);
 for (i in 1:length(genders)) {
   gender_group <- df.sw %>% filter(sex==genders[i]);
   cat(genders[i], ' age\n');
   report_col(gender_group$age);
 }
-# Male age: min: 24 | max: 58 | mean: 38.5 | sd: 11.4717
-# Female age: min: 27 | max: 57 | mean: 39.1111 | sd: 8.6832
+# Male age: min: 24 | max: 58 | mean: 41 | sd: 11.9739 
+# Female age: min: 25 | max: 64 | mean: 41.1765 | sd: 11.2596 
 
 # Task duration
 report_col(df.sw$task_duration)
-# min: 132,781 | max: 2,322,982 | mean: 352,052.5 | sd: 365,263.9
+# min: 132781 | max: 2322982 | mean: 360908.5 | sd: 362532.1 
 
 # Learn tasks
 summarise_group(df.sw$learningTaskId)
-# total 34 | learn04 6 17.65% | learn06 6 17.65% | learn05 7 20.59% | learn01 2 5.88% | learn02 13 38.24%
+# total 34 
+# learn01 4 11.76% 
+# learn02 5 14.71% 
+# learn03 6 17.65%
+# learn04 6 17.65% 
+# learn05 7 20.59% 
+# learn06 6 17.65% 
 
 # Out of curisity
-report_col(df.sw$difficulty) # min: 0 | max: 10 | mean: 4.8529 | sd: 2.7428 
-report_col(df.sw$engagement) # min: 2 | max: 10 | mean: 8.4706 | sd: 1.9577
+report_col(df.sw$difficulty) # min: 0 | max: 10 | mean: 4.8235 | sd: 2.9896 
+report_col(df.sw$engagement) # min: 2 | max: 10 | mean: 8.5882 | sd: 2.0906 
 
 # Take a peek of stats per learning condition groups
 lg_shape <- df.sw %>% filter(learningTaskId %in% c('learn01', 'learn02')) # groups with a shape-changing rule
 lg_color <- df.sw %>% filter(learningTaskId %in% c('learn03', 'learn04')) # groups with a color-changing rule
 lg_object <- df.sw %>% filter(learningTaskId %in% c('learn05', 'learn06')) # groups with color & shape changing rules
 
-nrow(lg_shape) # 15
-nrow(lg_color) # 6
+nrow(lg_shape) # 9
+nrow(lg_color) # 12
 nrow(lg_object) # 13
 
-report_col(lg_shape$task_duration)  # min: 166007 | max: 575022 | mean: 311988.5 | sd: 121420.6 
-report_col(lg_color$task_duration)  # min: 157104 | max: 370915 | mean: 276959.8 | sd: 74514.82
+report_col(lg_shape$task_duration)  # min: 218973 | max: 575022 | mean: 345779.4 | sd: 133882.9 
+report_col(lg_color$task_duration)  # min: 157104 | max: 370915 | mean: 294222.9 | sd: 64136.33 
 report_col(lg_object$task_duration) # min: 132781 | max: 2322982 | mean: 432938.5 | sd: 579199 
 
-report_col(lg_shape$difficulty)  # min: 0 | max: 10 | mean: 4.5333 | sd: 2.9488 
-report_col(lg_color$difficulty)  # min: 3 | max: 10 | mean: 6.1667 | sd: 2.4833 
-report_col(lg_object$difficulty) # min: 0 | max: 9 | mean: 4.6154 | sd: 2.6312
+report_col(lg_shape$difficulty)  # min: 0 | max: 10 | mean: 4.8889 | sd: 3.2189 
+report_col(lg_color$difficulty)  # min: 3 | max: 10 | mean: 5 | sd: 3.4112 
+report_col(lg_object$difficulty) # min: 0 | max: 9 | mean: 4.6154 | sd: 2.6312 
 
 lg_same <- df.sw %>% filter(learningTaskId %in% c('learn01', 'learn03', 'learn06')) # change to the same feature
 lg_diff <- df.sw %>% filter(learningTaskId %in% c('learn02', 'learn04','learn05')) # change to a different feature
 
-nrow(lg_same) # 8
-nrow(lg_diff) # 26
+nrow(lg_same) # 16
+nrow(lg_diff) # 18
 
-report_col(lg_same$task_duration) # min: 183647 | max: 575022 | mean: 319050.4 | sd: 126109.5
-report_col(lg_diff$task_duration) # min: 132781 | max: 2322982 | mean: 362207 | sd: 413766.4 
+report_col(lg_same$task_duration) # min: 183647 | max: 575022 | mean: 308112.8 | sd: 94710.8 
+report_col(lg_diff$task_duration) # min: 132781 | max: 2322982 | mean: 407838.1 | sd: 492196.9 
 
-report_col(lg_same$difficulty) # min: 0 | max: 9 | mean: 3.75 | sd: 3.1053 
-report_col(lg_diff$difficulty) # min: 0 | max: 10 | mean: 5.1923 | sd: 2.5926 
+report_col(lg_same$difficulty) # min: 0 | max: 10 | mean: 3.625 | sd: 3.2223 
+report_col(lg_diff$difficulty) # min: 0 | max: 10 | mean: 5.8889 | sd: 2.3736 
 
 
 # Take a look per learning task
@@ -104,8 +110,7 @@ check_task <- function(taskId) {
   trials <- df.tw %>% filter(id %in% ids[[1]])
   return(trials)
 }
-learn_1 <- check_task('learn01')
-
+inspect <- check_task('learn01')
 
 
 
