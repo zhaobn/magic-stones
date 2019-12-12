7775c68daa7dbac4ea64d6a369dbec2bb19f7644
bonan <zbn.dale@gmail.com>
2019-12-12 13:33:35 +0800

    :sparkles: Update hypothesis space with observations

--- modeling/normative_model/init.R
+++ modeling/normative_model/init.R
@@ -1,8 +1,8 @@
 
 ## Set up primitive elements
-colors <- c('b', 'r', 'y')
-shapes <- c('c', 'd', 's')
-objects <- vector('character')
+colors <- c('b', 'r', 'y') # blue, red, yellow
+shapes <- c('c', 'd', 's') # circle, diamond, square
+objects <- vector('character') # color-shape
 for (c in 1:length(colors)) {
   for (s in 1:length(shapes)) {
     objects <- c(objects, paste0(colors[c], shapes[s]))
@@ -46,8 +46,68 @@ full_hypothesis_space <- generate_hypo(shapes, colors, full_hypothesis_space)
 # (9) s -> s
 full_hypothesis_space <- generate_hypo(shapes, shapes, full_hypothesis_space)
 
-## Create theories
 
-## Create learning updates
+## Update from learnings
+# Exclude hypotheses that are incosistent with observations
+# according to the nine theories
+check_hypo <- function(observation, hypo) {
+  pass <- 0
+  if (observation[1] == hypo[1] && observation[3] == hypo[2]) {
+    pass <- 1 # (1) o -> o
+  } else if (observation[1] == hypo[1] && 
+             substr(hypo[2], 2, 2) == '*' && 
+             substr(observation[3], 1, 1) == substr(hypo[2], 1, 1)) {
+    pass <- 1 # (2) o -> c
+  } else if (observation[1] == hypo[1] &&
+             substr(hypo[2], 1, 1) == '*' &&
+             substr(observation[3], 2, 2) == substr(hypo[2], 2, 2)) {
+    pass <- 1 # (3) o -> s
+  } else if (substr(hypo[1], 2, 2) == '*' &&
+             substr(hypo[1], 1, 1) == substr(observation[1], 1, 1) && 
+             hypo[2] == observation[3]) {
+    pass <- 1 # (4) c -> o 
+  } else if (substr(hypo[1], 2, 2) == '*' &&
+             substr(hypo[1], 1, 1) == substr(observation[1], 1, 1) && 
+             substr(observation[3], 1, 1) == substr(hypo[2], 1, 1)) {
+    pass <- 1 # (5) c -> c
+  } else if (substr(hypo[1], 2, 2) == '*' &&
+             substr(hypo[1], 1, 1) == substr(observation[1], 1, 1) && 
+             substr(observation[3], 2, 2) == substr(hypo[2], 2, 2)) {
+    pass <- 1 # (6) c -> s
+  } else if (substr(hypo[1], 1, 1) == '*' &&
+             substr(hypo[1], 2, 2) == substr(observation[1], 2, 2) && 
+             hypo[2] == observation[3]) {
+    pass <- 1 # (7) s -> o
+  } else if (substr(hypo[1], 1, 1) == '*' &&
+             substr(hypo[1], 2, 2) == substr(observation[1], 2, 2) &&
+             substr(observation[3], 1, 1) == substr(hypo[2], 1, 1)) {
+    pass <- 1 # (8) s -> c
+  } else if (substr(hypo[1], 1, 1) == '*' &&
+             substr(hypo[1], 2, 2) == substr(observation[1], 2, 2) &&
+             substr(observation[3], 2, 2) == substr(hypo[2], 2, 2)) {
+    pass <- 1 # (9) s -> s
+  } 
+  return(pass)
+}
+
+# Get the filtered hypothesis space
+filter_hypos <- function(observation, space) {
+  passed <- list()
+  for (i in 1:length(space)) {
+    passed[[i]] <- check_hypo(observation, space[[i]])  
+  }
+  return(space[which(passed==1)])
+}
 
 ## Create generalization simulations
+# List the six learning scenarios
+learn01 <- c('rs', 'yc', 'ys')
+learn02 <- c('yd', 'rs', 'rc')
+learn03 <- c('bs', 'rd', 'bd')
+learn04 <- c('rc', 'bs', 'ys')
+learn05 <- c('yd', 'bs', 'yc')
+learn06 <- c('bs', 'yc', 'bs')
+
+h01 <- filter_hypos(learn01, full_hypothesis_space)
+h02 <- filter_hypos(learn02, full_hypothesis_space)
+
